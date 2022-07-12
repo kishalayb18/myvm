070e999fa8fe98447285bf40e861fedb82131a74
KISHALAY BHATTACHARYA <64585126+kishalayb18@users.noreply.github.com>
2022-07-12 14:41:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "default" {
-  name         = "my-vm-console-1"
+resource "google_compute_instance" "vm_instance" {
+  name         = "cf-vm-1"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
   allow_stopping_for_update = true
